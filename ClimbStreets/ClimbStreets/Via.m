9aff1c538614823dbd4f45a41a4b5ea2c288c1c3
Thiago <tl.domacoski@gmail.com>
2013-10-29 13:17:21 -0200

    include Via
    inclusao do objeto Via no aplicativo.

---
+++ ClimbStreets/ClimbStreets/Via.m
@@ -0,0 +1,13 @@
+//
+//  Via.m
+//  ClimbStreets
+//  Classe representa uma via para o sistema.
+//  Created by 4g on 10/29/13.
+//  Copyright (c) 2013 gennium. All rights reserved.
+//
+
+#import "Via.h"
+
+@implementation Via
+@synthesize name;
+@end
