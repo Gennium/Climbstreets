4945e340e3fed068a3ad22161ecdd3800cd68d16
Thiago <tl.domacoski@gmail.com>
2013-11-04 18:48:30 -0200

    implementacao da Visualizacao da Area

--- ClimbStreets/ClimbStreets/AbstractDAO.h
+++ ClimbStreets/ClimbStreets/AbstractDAO.h
@@ -2,13 +2,21 @@
 #import <CoreData/CoreData.h>
 @interface AbstractDAO : NSObject{
     NSString * table;
-    NSManagedObjectContext *managedObjectModel;
+    NSString * dataBase;
 }
 @property(nonatomic, retain) NSString * table;
+@property(nonatomic, retain) NSString * dataBase;
 @property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
-@property(nonatomic, retain) NSManagedObjectContext *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
--(id)initWithTable:(NSString*)table_;
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+-(id)initWithTable:(NSString*)table_ dataBase:(NSString*)dataBase_;
+
 
 -(NSEntityDescription*)insertNewObjectForEntity;
 -(NSEntityDescription*)getEntity;
---
+++ ClimbStreets/UserBO.h
@@ -0,0 +1,13 @@
+//
+//  UserBO.h
+//  ClimbStreets
+//
+//  Created by 4g on 11/4/13.
+//  Copyright (c) 2013 gennium. All rights reserved.
+//
+
+#import "AbstractDAO.h"
+
+@interface UserBO : AbstractDAO
+
+@end
